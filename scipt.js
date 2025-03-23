e99c97d9554264bace596b88a8c2e08dddb7da6b
Taylor Putman <fishputman@gmail.com>
2025-03-23 12:31:09 -0400

    added changing images to photo-grid

---
+++ scipt.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const images = [
+    {
+      id: "img-1",
+      hoverSrc: "imgs/DSC09248.JPG",
+      originalSrc: "imgs/IMG_7040.jpg",
+    },
+    {
+      id: "img-2",
+      hoverSrc: "imgs/DSC09139.JPG",
+      originalSrc: "imgs/IMG_3486.jpg",
+    },
+    {
+      id: "img-3",
+      hoverSrc: "imgs/DSC08971.JPG",
+      originalSrc: "imgs/6EFB35A9-3A89-4BC3-A1D7-2C0BAC718A45.jpg",
+    },
+  ];
+
+  images.forEach((image) => {
+    const imgElement = document.getElementById(image.id);
+
+    imgElement.addEventListener("mouseover", () => {
+      imgElement.src = imgElement.src.includes(image.hoverSrc)
+        ? image.originalSrc
+        : image.hoverSrc;
+    });
+  });
+});
